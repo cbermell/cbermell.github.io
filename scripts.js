b138bcc2e1c8948ae3161f793d28a5ffbb0b0ca9
cbermell <cbermell@espol.edu.ec>
2024-06-01 02:31:06 -0500

    ds

--- scripts.js
+++ scripts.js
@@ -1,13 +1,19 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    const navLinks = document.querySelectorAll('header nav ul li a');
-    
-    navLinks.forEach(link => {
-        link.addEventListener('mouseover', () => {
-            link.style.color = '#ff6347';
-        });
+document.addEventListener('DOMContentLoaded', function() {
+    const themeToggle = document.getElementById('theme-toggle');
+    const darkModeToggle = document.getElementById('dark-mode-toggle');
+    const body = document.body;
 
-        link.addEventListener('mouseout', () => {
-            link.style.color = '#fff';
-        });
+    // Cambiar entre modo claro y oscuro al hacer clic en el botón de alternar tema
+    themeToggle.addEventListener('click', function() {
+        body.classList.toggle('dark-mode');
+        const icon = darkModeToggle.querySelector('i');
+
+        if (body.classList.contains('dark-mode')) {
+            icon.classList.remove('fa-moon');
+            icon.classList.add('fa-sun');
+        } else {
+            icon.classList.remove('fa-sun');
+            icon.classList.add('fa-moon');
+        }
     });
 });
